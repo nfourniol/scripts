a8e485f2136fe77ccb76b875cf35cc10a0621bb8
Nicolas FOURNIOL <nfourniol@gmail.com>
2023-10-18 09:20:13 +0200

    Update detectPasswordExpiration.ps1

--- windows/activeDirectory/detectPasswordExpiration.ps1
+++ windows/activeDirectory/detectPasswordExpiration.ps1
@@ -19,19 +19,19 @@ for ($i=0; $i -lt $arrayExpiryUser.Length; $i++) {
         $mailTo = "{0} <{1}>" -f $arrayExpiryUser[$i]."DisplayName",$arrayExpiryUser[$i]."UserPrincipalName"
         $subject = "Votre mot de passe Windows expire le {0}" -f $arrayExpiryUser[$i]."ExpiryDate"
         $body=@"
-Le mot de passe de votre session Windows expire le $($arrayExpiryUser[$i]."ExpiryDate").<br/>
-Veuillez le changer lors de votre présence à YOURCOMPANY.<br/><br/>
-Vous ne pourrez pas le changer depuis votre accès VPN (c'est à dire en remote) $($EmojiIconWink)<br/>
+Your Windows session password will expire soon.<br/>
+Please change it when you come to OURCOMPANY.<br/><br/>
+You will not be able to change it from your VPN access (i.e. remotely). $($EmojiIconWink)<br/>
 
-Pour rappel, votre mot de passe doit respecter la stratégie suivante :<br>
+As a reminder, your password must comply with the following strategy:<br>
     <ul>
-        <li>12 caractères minimum</li>
-        <li>au moins 3 types de caractères différents parmi les types suivants : majuscule, minuscule, chiffre, caractères spéciaux</li>
+        <li>12 characters minimum</li>
+        <li>at least 3 different types of characters among the following types: upper case, lower case, number, special characters</li>
     </li>
     $($EmojiIconSmile)
 "@
         echo $mailTo
-        Send-MailMessage -From 'IT YOURCOMPANY <it@YOURCOMPANY.com>' -To "$($mailTo)" -Subject "$($subject)" -Body "$($body)" -BodyAsHtml -Priority High -DeliveryNotificationOption OnSuccess, OnFailure -SmtpServer 'smtp.yourcompany.com' -Encoding 'utf32'
+        Send-MailMessage -From 'IT OURCOMPANY <it@yourdomain.com>' -To "$($mailTo)" -Subject "$($subject)" -Body "$($body)" -BodyAsHtml -Priority High -DeliveryNotificationOption OnSuccess, OnFailure -SmtpServer 'smtp.yourcompany.com' -Encoding 'utf32'
     
     }
 }
